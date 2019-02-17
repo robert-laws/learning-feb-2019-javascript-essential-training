f62d57eafb30bfd1dfb79c05b04077fb789ed351
Robert Laws <roblaws@me.com>
2019-02-17 11:58:27 +0300

    setup for clock DOM example

---
+++ src/js/clock.js
@@ -0,0 +1,14 @@
+if (document.querySelector("#hour")) {
+  const HOURHAND = document.querySelector("#hour");
+  const MINUTEHAND = document.querySelector("#minute");
+  const SECONDHAND = document.querySelector("#second");
+
+  let hrPosition = 20;
+  let minPosition = 230;
+  let secPosition = 267;
+
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
