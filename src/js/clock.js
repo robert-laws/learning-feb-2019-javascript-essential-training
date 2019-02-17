3fd42cd99bb4ee11cb5c89cb05cb58ee1aa3603a
Robert Laws <roblaws@me.com>
2019-02-17 16:02:17 +0300

    finish clock DOM example

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,13 +1,36 @@
-if (document.querySelector("#hour")) {
-  const HOURHAND = document.querySelector("#hour");
-  const MINUTEHAND = document.querySelector("#minute");
-  const SECONDHAND = document.querySelector("#second");
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
 
-  let hrPosition = 20;
-  let minPosition = 230;
-  let secPosition = 267;
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds(); 
 
-  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = sec*360/60;
+
+function runTheClock() {
+  if(HOURHAND != undefined) {
+    hrPosition = hrPosition + (3/360)
+    minPosition = minPosition + (6/60);
+    secPosition = secPosition + 6
+  
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  }
 }
+
+const TITLE = document.querySelector("#clockTitle");
+
+function textAlert() {
+  if(TITLE != undefined) {
+    TITLE.classList.toggle("alert");
+  }
+}
+
+var interval = setInterval(runTheClock, 1000);
+
+var titleInterval = setInterval(textAlert, 2000);
