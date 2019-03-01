eb65f44b1ade377d8a7c2026eba17f975059406c
Robert Laws <roblaws@me.com>
2019-02-28 20:35:42 -0500

    synax correction

--- src/js/clock.js
+++ src/js/clock.js
@@ -13,9 +13,9 @@ let secPosition = sec*360/60;
 
 function runTheClock() {
   if(HOURHAND != undefined) {
-    hrPosition = hrPosition + (3/360)
+    hrPosition = hrPosition + (3/360);
     minPosition = minPosition + (6/60);
-    secPosition = secPosition + 6
+    secPosition = secPosition + 6;
   
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
